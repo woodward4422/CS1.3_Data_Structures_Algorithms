9499949222c64a659bd66dab113be0dd550e2e2a
noah <noahwoodward2244@gmail.com>
2019-04-13 17:21:47 -0700

    Added LL tests

--- string_algorithms/strings.py
+++ string_algorithms/strings.py
@@ -67,14 +67,15 @@ def contains_and_first_index(text, pattern):
 
 
 def test_string_algorithms(text, pattern):
+    print(text)
     # found = contains(text, pattern)
-    print('contains({!r}, {!r}) => {}'.format(text, pattern, found))
+    # print('contains({!r}, {!r}) => {}'.format(text, pattern, found))
     # TODO: Uncomment these lines after you implement find_index
     # index = find_index(text, pattern)
-    print('find_index({!r}, {!r}) => {}'.format(text, pattern, index))
+    # print('find_index({!r}, {!r}) => {}'.format(text, pattern, index))
     # TODO: Uncomment these lines after you implement find_all_indexes
     # indexes = find_all_indexes(text, pattern)
-    print('find_all_indexes({!r}, {!r}) => {}'.format(text, pattern, indexes))
+    # print('find_all_indexes({!r}, {!r}) => {}'.format(text, pattern, indexes))
 
 
 def main():
