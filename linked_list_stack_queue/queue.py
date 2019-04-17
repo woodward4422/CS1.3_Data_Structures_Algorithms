fce29c2f230803b29628be108e1343abb45ed8f1
noah <noahwoodward2244@gmail.com>
2019-04-16 22:32:02 -0700

    Finished the queue implementation

---
+++ linked_list_stack_queue/queue.py
@@ -0,0 +1,108 @@
+#!python
+
+from linkedlist import LinkedList
+
+
+# Implement LinkedQueue below, then change the assignment at the bottom
+# to use this Queue implementation to verify it passes all tests
+class LinkedQueue(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this queue and enqueue the given items, if any."""
+        # Initialize a new linked list to store the items
+        self.list = LinkedList()
+        if iterable is not None:
+            for item in iterable:
+                self.enqueue(item)
+
+    def __repr__(self):
+        """Return a string representation of this queue."""
+        return 'Queue({} items, front={})'.format(self.length(), self.front())
+
+    def is_empty(self):
+        """Return True if this queue is empty, or False otherwise."""
+        return self.list.is_empty()
+
+    def length(self):
+        """Return the number of items in this queue."""
+        return self.list.length()
+
+    def enqueue(self, item):
+        """Insert the given item at the back of this queue.
+        Running time: O(1) – Why? Appending to the end can be done in constant time with a tail property"""
+        self.list.append(item)
+
+    def front(self):
+        """Return the item at the front of this queue without removing it,
+        or None if this queue is empty."""
+        if self.is_empty():
+            return None
+        else:
+            return self.list.head.data
+
+    def dequeue(self):
+        """Remove and return the item at the front of this queue,
+        or raise ValueError if this queue is empty.
+        Running time: O(1) – Why? Removing from the beginning of the LL can be done in constant time with a reference to the head """
+        if self.is_empty():
+            raise ValueError("The Queue is empty!")
+        else:
+            front_element = self.front()
+            self.list.delete(front_element)
+            return front_element
+
+
+# Implement ArrayQueue below, then change the assignment at the bottom
+# to use this Queue implementation to verify it passes all tests
+class ArrayQueue(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this queue and enqueue the given items, if any."""
+        # Initialize a new list (dynamic array) to store the items
+        self.list = list()
+        if iterable is not None:
+            for item in iterable:
+                self.enqueue(item)
+
+    def __repr__(self):
+        """Return a string representation of this queue."""
+        return 'Queue({} items, front={})'.format(self.length(), self.front())
+
+    def is_empty(self):
+        """Return True if this queue is empty, or False otherwise."""
+        if len(self.list) == 0:
+            return True
+        else:
+            return False
+
+    def length(self):
+        """Return the number of items in this queue."""
+        return len(self.list)
+
+    def enqueue(self, item):
+        """Insert the given item at the back of this queue.
+        Running time: O(n) – Why? Prepending to the beginning requires a shift in the array"""
+        self.list.append(item)
+
+    def front(self):
+        """Return the item at the front of this queue without removing it,
+        or None if this queue is empty."""
+        if self.is_empty():
+            return None
+        else:
+            return self.list[0]
+
+    def dequeue(self):
+        """Remove and return the item at the front of this queue,
+        or raise ValueError if this queue is empty.
+        Running time: O(n) – Why? Removing an element at the beginning causes every element to shift down by one """
+        if self.is_empty():
+            raise ValueError("The Queue is empty!")
+        else:
+            return self.list.pop(0)
+
+
+# Implement LinkedQueue and ArrayQueue above, then change the assignment below
+# to use each of your Queue implementations to verify they each pass all tests
+# Queue = LinkedQueue
+Queue = ArrayQueue
